34cc9cfa9db71bf3fef9cad80a6ac59b0c4bf542
xiaocao <wdjoys@hotmail.com>
2022-04-27 16:50:37 +0800

    000

--- src/notification/wechat.py
+++ src/notification/wechat.py
@@ -2,7 +2,7 @@
 Author: wdjoys
 Date: 2022-04-23 12:37:27
 LastEditors: wdjoys
-LastEditTime: 2022-04-27 16:34:34
+LastEditTime: 2022-04-27 16:50:27
 FilePath: \guahao\src\notification\wechat.py
 Description: 
 
@@ -20,10 +20,12 @@ def send_notification(time, docName, other_information):
     msg_template = {
         "msgtype": "markdown",
         "markdown": {
-            "content": f"""⏱号源状态监测提醒：发现<font color=\"warning\">{docName}</font>医生，在<font color=\"warning\">{time}</font>有最新号源状态。\n
-                         >费用：<font color=\"comment\">{other_information['amount']}</font> 
-                         >等级：<font color=\"comment\">{other_information['registLevel']}</font> 
-                         >状态：<font color=\"comment\">{other_information['message']}</font><br>
+            "content": f"""⏱ 号源状态监测提醒：发现<font color=\"warning\">{docName}</font>医生，在<font color=\"warning\">{time}</font>有最新号源状态。
+
+> 费用：<font color=\"comment\">{other_information['amount']}</font>
+> 等级：<font color=\"comment\">{other_information['registLevel']}</font>
+> 状态：<font color=\"comment\">{other_information['message']}</font>
+
 前往支付：[点击这里]({other_information['payUrl']})"""
         }
     }
---
+++ xx.md
@@ -0,0 +1,18 @@
+<!--
+ * @Author: wdjoys
+ * @Date: 2022-04-27 16:48:59
+ * @LastEditors: wdjoys
+ * @LastEditTime: 2022-04-27 16:49:01
+ * @FilePath: \guahao\xx.md
+ * @Description:
+ *
+ * Copyright (c) 2022 by github/wdjoys, All Rights Reserved.
+-->
+
+⏱ 号源状态监测提醒：发现<font color=\"warning\">{docName}</font>医生，在<font color=\"warning\">{time}</font>有最新号源状态。
+
+> 费用：<font color=\"comment\">{other_information['amount']}</font>
+> 等级：<font color=\"comment\">{other_information['registLevel']}</font>
+> 状态：<font color=\"comment\">{other_information['message']}</font>
+
+前往支付：[点击这里]({other_information['payUrl']})
